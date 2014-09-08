f70d3ce0891397fbf2b2b2705e6f775a5024f45d
qcfrodon <mariobros_ch@hotmail.fr>
2014-09-08 15:08:43 -0400

    swag
    #yoloswag

---
+++ battleship/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace battleship
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GridAttack = new System.Windows.Forms.DataGridView();
+            this.col1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GridPlayer = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BTN_Attack = new System.Windows.Forms.Button();
+            this.BTN_NouvellePartie = new System.Windows.Forms.Button();
+            this.BTN_Quit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.GridAttack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GridPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GridAttack
+            // 
+            this.GridAttack.AllowUserToAddRows = false;
+            this.GridAttack.AllowUserToDeleteRows = false;
+            this.GridAttack.AllowUserToResizeColumns = false;
+            this.GridAttack.AllowUserToResizeRows = false;
+            this.GridAttack.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridAttack.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.col1,
+            this.col2,
+            this.col3,
+            this.col4,
+            this.col5,
+            this.col6,
+            this.col7,
+            this.col8,
+            this.col9,
+            this.col10});
+            this.GridAttack.Location = new System.Drawing.Point(23, 12);
+            this.GridAttack.MultiSelect = false;
+            this.GridAttack.Name = "GridAttack";
+            this.GridAttack.ReadOnly = true;
+            this.GridAttack.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.GridAttack.Size = new System.Drawing.Size(568, 244);
+            this.GridAttack.TabIndex = 0;
+            // 
+            // col1
+            // 
+            this.col1.HeaderText = "1";
+            this.col1.Name = "col1";
+            this.col1.ReadOnly = true;
+            this.col1.Width = 50;
+            // 
+            // col2
+            // 
+            this.col2.HeaderText = "2";
+            this.col2.Name = "col2";
+            this.col2.ReadOnly = true;
+            this.col2.Width = 50;
+            // 
+            // col3
+            // 
+            this.col3.HeaderText = "3";
+            this.col3.Name = "col3";
+            this.col3.ReadOnly = true;
+            this.col3.Width = 50;
+            // 
+            // col4
+            // 
+            this.col4.HeaderText = "4";
+            this.col4.Name = "col4";
+            this.col4.ReadOnly = true;
+            this.col4.Width = 50;
+            // 
+            // col5
+            // 
+            this.col5.HeaderText = "5";
+            this.col5.Name = "col5";
+            this.col5.ReadOnly = true;
+            this.col5.Width = 50;
+            // 
+            // col6
+            // 
+            this.col6.HeaderText = "6";
+            this.col6.Name = "col6";
+            this.col6.ReadOnly = true;
+            this.col6.Width = 50;
+            // 
+            // col7
+            // 
+            this.col7.HeaderText = "7";
+            this.col7.Name = "col7";
+            this.col7.ReadOnly = true;
+            this.col7.Width = 50;
+            // 
+            // col8
+            // 
+            this.col8.HeaderText = "8";
+            this.col8.Name = "col8";
+            this.col8.ReadOnly = true;
+            this.col8.Width = 50;
+            // 
+            // col9
+            // 
+            this.col9.HeaderText = "9";
+            this.col9.Name = "col9";
+            this.col9.ReadOnly = true;
+            this.col9.Width = 50;
+            // 
+            // col10
+            // 
+            this.col10.HeaderText = "10";
+            this.col10.Name = "col10";
+            this.col10.ReadOnly = true;
+            this.col10.Width = 50;
+            // 
+            // GridPlayer
+            // 
+            this.GridPlayer.AllowUserToAddRows = false;
+            this.GridPlayer.AllowUserToDeleteRows = false;
+            this.GridPlayer.AllowUserToResizeColumns = false;
+            this.GridPlayer.AllowUserToResizeRows = false;
+            this.GridPlayer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridPlayer.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6,
+            this.dataGridViewTextBoxColumn7,
+            this.dataGridViewTextBoxColumn8,
+            this.dataGridViewTextBoxColumn9,
+            this.dataGridViewTextBoxColumn10});
+            this.GridPlayer.Location = new System.Drawing.Point(23, 262);
+            this.GridPlayer.MultiSelect = false;
+            this.GridPlayer.Name = "GridPlayer";
+            this.GridPlayer.ReadOnly = true;
+            this.GridPlayer.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.GridPlayer.Size = new System.Drawing.Size(568, 244);
+            this.GridPlayer.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "1";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            this.dataGridViewTextBoxColumn1.Width = 50;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "2";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            this.dataGridViewTextBoxColumn2.Width = 50;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "3";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
+            this.dataGridViewTextBoxColumn3.Width = 50;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.HeaderText = "4";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.ReadOnly = true;
+            this.dataGridViewTextBoxColumn4.Width = 50;
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.HeaderText = "5";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            this.dataGridViewTextBoxColumn5.ReadOnly = true;
+            this.dataGridViewTextBoxColumn5.Width = 50;
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.HeaderText = "6";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            this.dataGridViewTextBoxColumn6.ReadOnly = true;
+            this.dataGridViewTextBoxColumn6.Width = 50;
+            // 
+            // dataGridViewTextBoxColumn7
+            // 
+            this.dataGridViewTextBoxColumn7.HeaderText = "7";
+            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
+            this.dataGridViewTextBoxColumn7.ReadOnly = true;
+            this.dataGridViewTextBoxColumn7.Width = 50;
+            // 
+            // dataGridViewTextBoxColumn8
+            // 
+            this.dataGridViewTextBoxColumn8.HeaderText = "8";
+            this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
+            this.dataGridViewTextBoxColumn8.ReadOnly = true;
+            this.dataGridViewTextBoxColumn8.Width = 50;
+            // 
+            // dataGridViewTextBoxColumn9
+            // 
+            this.dataGridViewTextBoxColumn9.HeaderText = "9";
+            this.dataGridViewTextBoxColumn9.Name = "dataGridViewTextBoxColumn9";
+            this.dataGridViewTextBoxColumn9.ReadOnly = true;
+            this.dataGridViewTextBoxColumn9.Width = 50;
+            // 
+            // dataGridViewTextBoxColumn10
+            // 
+            this.dataGridViewTextBoxColumn10.HeaderText = "10";
+            this.dataGridViewTextBoxColumn10.Name = "dataGridViewTextBoxColumn10";
+            this.dataGridViewTextBoxColumn10.ReadOnly = true;
+            this.dataGridViewTextBoxColumn10.Width = 50;
+            // 
+            // BTN_Attack
+            // 
+            this.BTN_Attack.Location = new System.Drawing.Point(598, 33);
+            this.BTN_Attack.Name = "BTN_Attack";
+            this.BTN_Attack.Size = new System.Drawing.Size(106, 44);
+            this.BTN_Attack.TabIndex = 2;
+            this.BTN_Attack.Text = "Attack";
+            this.BTN_Attack.UseVisualStyleBackColor = true;
+            this.BTN_Attack.Click += new System.EventHandler(this.BTN_Attack_Click);
+            // 
+            // BTN_NouvellePartie
+            // 
+            this.BTN_NouvellePartie.Location = new System.Drawing.Point(816, 415);
+            this.BTN_NouvellePartie.Name = "BTN_NouvellePartie";
+            this.BTN_NouvellePartie.Size = new System.Drawing.Size(134, 38);
+            this.BTN_NouvellePartie.TabIndex = 3;
+            this.BTN_NouvellePartie.Text = "Nouvelle Partie";
+            this.BTN_NouvellePartie.UseVisualStyleBackColor = true;
+            // 
+            // BTN_Quit
+            // 
+            this.BTN_Quit.Location = new System.Drawing.Point(815, 459);
+            this.BTN_Quit.Name = "BTN_Quit";
+            this.BTN_Quit.Size = new System.Drawing.Size(135, 38);
+            this.BTN_Quit.TabIndex = 4;
+            this.BTN_Quit.Text = "Quitter";
+            this.BTN_Quit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(962, 509);
+            this.Controls.Add(this.BTN_Quit);
+            this.Controls.Add(this.BTN_NouvellePartie);
+            this.Controls.Add(this.BTN_Attack);
+            this.Controls.Add(this.GridPlayer);
+            this.Controls.Add(this.GridAttack);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.GridAttack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GridPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView GridAttack;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col10;
+        private System.Windows.Forms.DataGridView GridPlayer;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn10;
+        private System.Windows.Forms.Button BTN_Attack;
+        private System.Windows.Forms.Button BTN_NouvellePartie;
+        private System.Windows.Forms.Button BTN_Quit;
+
+    }
+}
+
---
+++ battleship/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace battleship
+{
+    
+    public partial class Form1 : Form
+    {
+        String [] ligneHeader={"A","B","C","D","E","F","G","H","I","J"}; 
+        public Form1()
+        {
+            InitializeComponent();
+            createGrid();
+        }
+        private void createGrid() {
+            for (int i = 0; i < GridAttack.ColumnCount; ++i) {
+                GridAttack.Rows.Add();              
+                GridAttack.Rows[i].HeaderCell.Value = ligneHeader[i];
+                GridPlayer.Rows.Add();
+                GridPlayer.Rows[i].HeaderCell.Value = ligneHeader[i];
+            }
+        }
+
+        private void BTN_Attack_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
