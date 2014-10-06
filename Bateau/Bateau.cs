81f439d27030e6ccc7847f1711edafd56821fa15
qcfrodon <mariobros_ch@hotmail.fr>
2014-10-06 14:08:13 -0400

    #comment

--- Bateau/Bateau.cs
+++ Bateau/Bateau.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace BateauDLL
 {
     [Serializable]
+    //Classe position contient un position en X et en Y.
     public class position
     {
         public int x;
@@ -22,10 +23,13 @@ namespace BateauDLL
     [Serializable]
     public class Bateau
     {
+        // classe du bateau, contient la longueur du bateaux (2,3,4 ou 5),son nom, leur position sur une matrice (x, y)
+        // et s'il est touché.
         public int longueur { get; set; }
         public String nom { get; set; }
         public position [] cases;
         public bool [] estTouche;
+        //constructeur paramétrique de bateau: Recoit la longueur (l) et le nom du bateau (N).
         public Bateau(int l, String N)
         {
             longueur = l;
@@ -40,6 +44,7 @@ namespace BateauDLL
     [Serializable]
     public class Joueur
     {
+        //classe du joueur, contient le nom du joueur ( joueur 1 ou joueur 2) et les bateaux.
         public String Nom_;
         public Bateau PorteAvion;
         public Bateau Croiseur;
@@ -47,6 +52,7 @@ namespace BateauDLL
         public Bateau SousMarin;
         public Bateau Torpilleur;
 
+        //Constructeur paramétrique de joueur : Recoit le nom du joueur (joueur 1 ou joueur 2).
         public Joueur(String Nom) {
             Nom_ = Nom;
             PorteAvion = new Bateau(5, "Porte-Avion");
